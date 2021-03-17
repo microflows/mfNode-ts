a9a529f828dd78be2915ef11e8ab22d295a9746e
sovlookup <gonorth@qq.com>
2021-03-17 15:33:39 +0800

    ssh

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,6 +1,7 @@
 const readlineSync = require('readline-sync')
 const shell = require('shelljs')
 const fs = require('fs')
+const iniparser = require('iniparser')
 
 // ensure git is installed
 shell.echo('\x1B[36mGit Version:\x1B[0m')
@@ -34,6 +35,12 @@ if (!fs.existsSync('build')) {
 const metadata = JSON.parse(fs.readFileSync('build/node.json'))
 const name = metadata.name
 const version = metadata.version
+
+// todo 从git文件中获取
+console.log(
+
+  iniparser.parseSync('.git/config')
+)
 const git = metadata.urls[0] || ''
 const commitMessage = name + ': ' + version
 
@@ -43,88 +50,93 @@ const cloud = config.cloud
 
 const currentVersion = '0.0.0'
 
-// ensure the right repo url todo:with array
-if (
-  readlineSync.question(
-    '\x1B[36mIs this your git repo address?(y/n)\x1B[0m ↓↓↓\n' +
-      git
-        .replace('cdn.jsdelivr.net/gh', 'github.com')
-        .replace('@release/release/index.js', '') +
-      '\n'
-  ) !== 'y'
-) {
-  shell.echo('Sorry, please write the right repo url!')
-  shell.exit(1)
-}
-shell.echo()
-
-// ensure version todo
-shell.echo('\x1B[36mEnsure the version right:\x1B[0m')
-shell.echo(currentVersion, version)
-// version >
-shell.echo()
-
-// ---
-shell.mv('build', 'release')
-
-// commit to current branch
-shell.echo('\x1B[36mCommit to current branch:\x1B[0m')
-shell.exec("git add . && git commit -m '" + commitMessage + "'")
-shell.echo()
-
-// push to current branch
-shell.echo('\x1B[36mPush to current branch:\x1B[0m')
-shell.exec("git push")
-shell.echo()
-
-// Switch to release branch
-shell.echo('\x1B[36mSwitch to release branch:\x1B[0m')
-shell.exec('git config advice.addIgnoredFile false')
-if (shell.exec('git checkout release').code !== 0) {
+function main() {
+  // ensure the right repo url todo:with array
   if (
-    shell.exec('git checkout -b release && git remote add release ' + git)
-      .code !== 0
+    readlineSync.question(
+      '\x1B[36mIs this your git repo address?(y/n)\x1B[0m ↓↓↓\n' +
+        git
+          .replace('cdn.jsdelivr.net/gh', 'github.com')
+          .replace('@release/release/index.js', '') +
+        '\n'
+    ) !== 'y'
   ) {
+    shell.echo('Sorry, please write the right repo url!')
     shell.exit(1)
   }
-}
-shell.echo()
+  shell.echo()
 
-// Merge master to release branch
-shell.echo('\x1B[36mMerge master to release branch:\x1B[0m')
-if (
-  shell.exec('git merge master && git push --set-upstream release release')
-    .code !== 0
-) {
+  // ensure version todo
+  shell.echo('\x1B[36mEnsure the version right:\x1B[0m')
+  shell.echo(currentVersion, version)
+  // version >
+  shell.echo()
+
+  // ---
+  shell.mv('build', 'release')
+
+  // commit to current branch
+  shell.echo('\x1B[36mCommit to current branch:\x1B[0m')
+  shell.exec("git add . && git commit -m '" + commitMessage + "'")
+  shell.echo()
+
+  // push to current branch
+  shell.echo('\x1B[36mPush to current branch:\x1B[0m')
+  shell.exec('git push')
+  shell.echo()
+
+  // Switch to release branch
+  shell.echo('\x1B[36mSwitch to release branch:\x1B[0m')
+  shell.exec('git config advice.addIgnoredFile false')
+  if (shell.exec('git checkout release').code !== 0) {
+    if (
+      shell.exec('git checkout -b release && git remote add release ' + git)
+        .code !== 0
+    ) {
+      shell.exit(1)
+    }
+  }
+  shell.echo()
+
+  // Merge master to release branch
+  shell.echo('\x1B[36mMerge master to release branch:\x1B[0m')
+  if (
+    shell.exec('git merge master && git push --set-upstream release release')
+      .code !== 0
+  ) {
+    shell.exec('git checkout ' + currentBranch)
+    shell.mv('release', 'build')
+    shell.echo('Push failed!')
+    shell.exit(1)
+  }
+  shell.echo()
+
+  // upload metadata todo
+  shell.echo('\x1B[36mUpload metadata:\x1B[0m')
+  console.log('upload...')
+  shell.echo()
+
+  // back to branch
+  shell.echo('\x1B[36mBack to ' + currentBranch + ' branch:\x1B[0m')
   shell.exec('git checkout ' + currentBranch)
+  shell.echo()
+
   shell.mv('release', 'build')
-  shell.echo('Push failed!')
-  shell.exit(1)
+  // ---
+
+  // Clean the dir
+  shell.echo('\x1B[36mClean the dir:\x1B[0m')
+  shell.exec(
+    "git rm -r release && git commit -m '" + commitMessage + "' && git push"
+  )
+  shell.echo()
+
+  // print urls
+  shell.echo(
+    "\x1B[36mYour release has been upload to cloud, your mfNode's cdn address:\x1B[0m" +
+      '\n\t' +
+      metadata.urls.join('\n')
+  )
+  shell.echo()
 }
-shell.echo()
-
-// upload metadata todo
-shell.echo('\x1B[36mUpload metadata:\x1B[0m')
-console.log('upload...')
-shell.echo()
-
-// back to branch
-shell.echo('\x1B[36mBack to ' + currentBranch + ' branch:\x1B[0m')
-shell.exec('git checkout ' + currentBranch)
-shell.echo()
-
-shell.mv('release', 'build')
-// ---
-
-// Clean the dir
-shell.echo('\x1B[36mClean the dir:\x1B[0m')
-shell.exec("git rm -r release && git commit -m '" + commitMessage + "' && git push")
-shell.echo()
-
-// print urls
-shell.echo(
-  "\x1B[36mYour release has been upload to cloud, your mfNode's cdn address:\x1B[0m" +
-    '\n\t' +
-    metadata.urls.join('\n')
-)
-shell.echo()
+// main()
