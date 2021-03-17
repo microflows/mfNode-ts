a4f315f831b34eb6571c75326c722d812f02895b
sovlookup <gonorth@qq.com>
2021-03-17 10:32:11 +0800

    hello: 0.0.1

--- scripts/publish.js
+++ scripts/publish.js
@@ -8,9 +8,19 @@ const fs = require('fs')
 const cloud = config.cloud
 const currentBranch = shell.exec("git branch --show-current").toString().replace("\n","")
 
+if (!fs.existsSync("build")) {
+  shell.echo('You should run build before publish!')
+  shell.exit(1)
+}
+const metadata = JSON.parse(fs.readFileSync("build/node.json"))
+
+const name = metadata.name
+const version = metadata.version
+const commitMessage = name + ": " + version
+
 // get branch
 if (
-shell.exec("git add . && git commit -m 'autocommit'").code !== 0) {
+shell.exec("git add . && git commit -m '" + commitMessage + "'").code !== 0) {
   shell.echo('Sorry this script need git!')
   shell.exit(1)
 }
@@ -23,33 +33,26 @@ if (
       }
   }
 
-if (!fs.existsSync("build")) {
-  shell.echo('You should run build before publish!')
-  shell.exit(1)
-}
 
 shell.mv("build","release")
 
 //   ensure version todo
 
 
-// git add && push todo:msg todo:add remote repourl
 if (
-    shell.exec('git add . && git commit -m "msg" && git push').code !== 0
+    shell.exec("git add . && git commit -m '" + commitMessage + "' && git push").code !== 0
   ) {
+      
         shell.exit(1)
   }
 
-
 //   upload metadata todo
-JSON.parse(fs.readFileSync("build/node.json"))
+// fetch post to cloud
+console.log("upload...")
 
 shell.mv("release","build")
 
 // back to branch
 shell.exec('git checkout ' + currentBranch)
 
-// return print url
-console.log(
-    shell.pwd().toString()
-)
+shell.echo("Your release has been upload to cloud, your mfNode's cdn address:"+ "\n" + metadata.urls.join("\n"))
