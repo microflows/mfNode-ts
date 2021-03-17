8d10158f4e7997540434d0961351482ef52174cb
sovlookup <gonorth@qq.com>
2021-03-17 19:32:11 +0800

    hello: 0.0.1

--- scripts/release.js
+++ scripts/release.js
@@ -118,7 +118,7 @@ function gitPush(branchs) {
     shell.echo('\x1B[36mMerge master to ' + branch + ' branch:\x1B[0m')
     if (
       shell.exec(
-        'git merge master && git push --set-upstream release'
+        'git merge master && git push --set-upstream release --tags'
       ).code !== 0
     ) {
       shell.exec('git checkout ' + currentBranch)
